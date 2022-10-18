462f6bdcddf5142d5ea6ccea9651fa6643b5dfc1
Ko-GyeongTae <kokt360@gmail.com>
2022-10-19 00:21:15 +0900

    limiter 추가

--- src/setting.ts
+++ src/setting.ts
@@ -3,10 +3,14 @@ import cors from "cors"
 import helmet from "helmet"
 import express from "express"
 import routes from "./routes"
+import pino from "express-pino-logger"
+import { limiter } from "./public/utils/express"
 
 export const settingServer = () => {
   const expressServer = express()
 
+  expressServer.use(limiter())
+  // expressServer.use(pino())
   expressServer.use(cors())
   expressServer.use(helmet())
   expressServer.use(express.json())
