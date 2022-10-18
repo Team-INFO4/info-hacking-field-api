60ad3cbd8f4d32c7d27272666752ac1d438a0d63
Ko-GyeongTae <kokt360@gmail.com>
2022-10-19 00:20:57 +0900

    limiter 함수 추가

--- src/public/utils/express.ts
+++ src/public/utils/express.ts
@@ -1,1 +1,12 @@
+import rateLimit from "express-rate-limit"
+import { HTTP_MESSAGE } from "../constants/http"
+
 export const okJson = { message: "ok" }
+
+export const limiter = () => {
+  return rateLimit({
+    max: 1,
+    windowMs: 1000,
+    message: { error: HTTP_MESSAGE.TOO_MANY_REQUEST },
+  })
+}
