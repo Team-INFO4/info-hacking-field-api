8bace1728a58c83ab4fe1487fffb24904cc19e90
Ko-GyeongTae <kokt360@gmail.com>
2022-10-23 16:28:09 +0900

    unused code 삭제

--- src/services/healthz/liveness.ts
+++ src/services/healthz/liveness.ts
@@ -1,7 +1,4 @@
-import createHttpError from "http-errors"
-import BadRequest from "http-errors"
 import { okJson } from "../../public/utils/express"
-import { AppForbidden, BadRequestttxwxw } from "../../public/utils/http-error"
 
 export const getLiveness = () => {
   return okJson
