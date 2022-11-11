cf6b67c6744a94511dc9967bd8f47078cf7a451b
Ko-GyeongTae <kokt360@gmail.com>
2022-11-11 19:32:20 +0900

    signUp service 추가

--- src/services/auth/sign-up.ts
+++ src/services/auth/sign-up.ts
@@ -1,14 +1,10 @@
 import { HackingField } from '@team-info4/info-models'
+import { SignUpPayload } from 'auth'
 import createHttpError from 'http-errors'
 import { okJson } from '../../public/utils/express'
 import { convertToSHA512 } from './helper'
 
-export const signUp = async (
-  payload: Pick<
-    HackingField.User,
-    'name' | 'content' | 'password' | 'email' | 'type'
-  >,
-) => {
+export const signUp = async (payload: SignUpPayload) => {
   const { name, content, password, email, type } = payload
 
   const isExist = await HackingField.User.findOne({
@@ -18,12 +14,12 @@ export const signUp = async (
   })
 
   if (isExist) {
-    return new createHttpError.BadRequest('이미 계정이 존재합니다.')
+    throw new createHttpError.BadRequest('이미 계정이 존재합니다.')
   }
 
   const encrypted = convertToSHA512(password)
 
-  const user = await HackingField.User.create({
+  await HackingField.User.create({
     name,
     content,
     email,
