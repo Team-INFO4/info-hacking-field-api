bd9665d268bc1d6df7c1794ea6d18c3cb3361b18
Ko-GyeongTae <kokt360@gmail.com>
2022-11-11 19:32:36 +0900

    api endpoint 수정

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,18 +1,23 @@
+import { signIn } from './../services/auth/sign-in'
 import { NextFunction, Request, Response, Router } from 'express'
 import { body } from 'express-validator'
 import validator from './'
 import { okJson } from '../public/utils/express'
 import { signUp } from '../services/auth'
 import { validate } from '../public/utils/validate'
+import { UserType } from '@team-info4/info-models/dist/models/hackingField/user.model'
 
 const url = '/auth'
 const router = Router()
 
 router.post(
   '/sign-in',
+  validate([body('email').isEmail().isString(), body('password').isString()]),
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      res.json(okJson)
+      const data = await signIn(req.body)
+
+      res.json({ data })
     } catch (err) {
       return next(err)
     }
@@ -21,7 +26,13 @@ router.post(
 
 router.post(
   '/sign-up',
-  validate([body('test').exists()]),
+  validate([
+    body('email').exists().isEmail().isString(),
+    body('name').exists().isString(),
+    body('content').exists().isString(),
+    body('type').exists().isIn(['USER', 'TEAM']),
+    body('password').exists().isString(),
+  ]),
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       const data = await signUp(req.body)
