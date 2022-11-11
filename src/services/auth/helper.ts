7ccc7f004d700bf26d7476b4ab7b81fe32c2c2ae
Ko-GyeongTae <kokt360@gmail.com>
2022-11-11 19:31:32 +0900

    jwt 함수 추가

--- src/services/auth/helper.ts
+++ src/services/auth/helper.ts
@@ -1,7 +1,29 @@
+import { HackingField } from '@team-info4/info-models'
 import crypto from 'crypto'
+import jwt, { JwtPayload } from 'jsonwebtoken'
 
-export const convertToSHA512 = (payload: string): string => {
+export const convertToSHA512 = (payload: string) => {
   const encrypted = crypto.createHash('sha512').update(payload).digest('hex')
 
   return encrypted
+}
+
+export const signAccessJwt = (payload: JwtPayload) => {
+  return jwt.sign(payload, process.env.SALT || 'test', {
+    algorithm: 'HS512',
+    expiresIn: '3d',
+  })
+}
+
+export const signRefreshJwt = (payload: string) => {
+  return jwt.sign(payload, process.env.SALT || 'test', {
+    algorithm: 'HS512',
+    expiresIn: '14d',
+  })
+}
+
+export const verifyJwt = (payload: string) => {
+  return jwt.verify(payload, process.env.SALT || 'test', {
+    algorithms: ['HS512'],
+  })
 }
